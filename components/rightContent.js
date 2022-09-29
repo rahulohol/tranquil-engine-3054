813072f17b2b1f3efb121e1e3ba37528701f6d9c
Rahul Ohol <nevergiupyou123@gmail.com>
2022-09-29 23:15:58 +0530

    ind

---
+++ components/rightContent.js
@@ -0,0 +1,63 @@
+export function rightContent() {
+    let div = document.createElement('div')
+    div.setAttribute('id', 'rightContent')
+    div.innerHTML = ` <div style="width: 100%;height: auto; border-radius: 7px;">
+    <h5 style="padding: 15px 16px 0px 16px;">Key Series</h5>
+    <hr>
+    <div style="padding: 5px;">
+        <li><a href="">Aisa Cup QLF</a></li>
+        <li><a href="">Aisa Cup</a></li>
+        <li><a href="">England v South Africa</a></li>
+        <li><a href="">Austalia v Zimbabwe</a></li>
+        <li><a href="">NZ-A in IND</a></li>
+        <li><a href="">The Hundred(M)</a></li>
+        <li><a href="">The Hundred(W)</a></li>
+        <li><a href="">Country Div1</a></li>
+        <li><a href="">Country Div2</a></li>
+        <li><a href="">6IXTY(M)</a></li>
+        <li><a href="">6IXTY(W)</a></li>
+        <li><a href="">Women's Championship</a></li>
+        <li><a href="">World Test Championship</a></li>
+        <li><a href="">World Cup League</a></li>
+    </div>
+</div>
+<div style="width: 100%;height: auto; border-radius: 7px;">
+    <h5 style="padding: 15px 16px 0px 16px;">Quick Links</h5>
+    <hr>
+    <div style="padding: 5px;">
+        <li><a href="">T20 Time Out</a></li>
+        <li><a href="">T20 Time Out Hindi</a></li>
+        <li><a href="">ICC Rankings</a></li>
+        <li><a href="">Fantasy Pick</a></li>
+        <li><a href="">Haan Ya Naa </a></li>
+    </div>
+</div>
+<div style="width: 100%;height: auto; border-radius: 7px;">
+    <h5 style="padding: 15px 16px 0px 16px;">ESPNcricinfo Apps</h5>
+    <hr>
+    <div style="padding: 5px;">
+        <li><a href="">Android App</a></li>
+        <li><a href="">iOS App</a></li>
+    </div>
+</div>
+<div style="width: 100%;height: auto; border-radius: 7px;">
+    <h5 style="padding: 15px 16px 0px 16px;">Follow ESPNcricinfo</h5>
+    <hr>
+    <div style="padding: 5px;">
+        <li><a href="">YouTube</a></li>
+        <li><a href="">Facebook</a></li>
+        <li><a href="">Twitter</a></li>
+        <li><a href="">Instagram</a></li>
+    </div>
+</div>
+<div style="width: 100%;height: auto; border-radius: 7px;">
+    <h5 style="padding: 15px 16px 0px 16px;">ESPN Sites</h5>
+    <hr>
+    <div style="padding: 5px;">
+        <li><a href="">The Cricket Monthly</a></li>
+        <li><a href="">ESPN</a></li>
+    </div>
+</div>`
+
+    return div
+}
